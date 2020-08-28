129d4255b18ffd45ffee586b8e6988fd00438578
CHOIDONGGYUN <chdg2851@gmail.com>
2020-08-28 23:45:49 +0900

    0828

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './componets/Customer';
+// 기동시 yarn start쓸것
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'http://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '9612222',
+  'gender' : '남자',
+  'job' : '회사원'
+  },
+  {
+    'id' : 2,
+    'image' : 'http://placeimg.com/64/64/1',
+    'name' : '겐지충',
+    'birthday' : '8812022',
+    'gender' : '여자',
+    'job' : '회사원'
+    },
+    {
+      'id' : 3,
+      'image' : 'http://placeimg.com/64/64/2',
+      'name' : '으음동',
+      'birthday' : '9612222',
+      'gender' : '남자',
+      'job' : '학생'
+      },
+]
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div>
+        {
+          // 반드시 맵을쓸때는 key를 넣을꺼!! 안넣으면 오류 발생함. 
+          customers.map( c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
